a4e3bcbeff95986b7eb19e192576cdf14a9ca02d
Clarence Manuel <rencedm112@gmail.com>
2023-02-20 05:05:11 +0800

    Update setup file

--- setup/fedora/core.sh
+++ setup/fedora/core.sh
@@ -1,29 +1,34 @@
-# stores package to executable map
-declare -A core_dnf_packages
+# stores executable to package map
+declare -A dnf_packages
 
-core_dnf_packages["neovim"]="nvim"
-core_dnf_packages["git"]="git"
-core_dnf_packages["clang"]="clang"
-core_dnf_packages["cmake"]="cmake"
-core_dnf_packages["snapd"]="snap" 
+dnf_packages["nvim"]="neovim"
+dnf_packages["git"]="git"
+dnf_packages["clang"]="clang"
+dnf_packages["cmake"]="cmake"
+dnf_packages["snap"]="snapd"
+dnf_packages["shmft"]="shfmt"
 
+declare -A external_binaries
+
+declare -A flatpak_packages
 
 declare -A post_install_commands
 
-post_install_commands["snapd"]="sudo ln -s /var/lib/snapd/snap /snap"
+post_install_commands["snap"]="sudo ln -s /var/lib/snapd/snap /snap"
 
 to_be_installed=()
 to_be_run=()
 
 # The list which constains packages to install
 
-for package in "${!core_dnf_packages[@]}"; do
-  if ! which "${core_dnf_packages[${package}]}" &> /dev/null; then
-    to_be_installed+=("${package}")
-    to_be_run+=("${post_install_commands[${package}]}")
+for executable in "${!dnf_packages[@]}"; do
+	package="${dnf_packages[${executable}]}"
+	if ! which "${executable}" &>/dev/null; then
+		to_be_installed+=("${package}")
+		to_be_run+=("${post_install_commands[${executable}]}")
 
-    echo "${package} will be installed"
-  else 
-    echo "${package} is already installed"
-  fi
+		echo "${package} will be installed"
+	else
+		echo "${package} is already installed"
+	fi
 done
