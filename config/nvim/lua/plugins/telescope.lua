a824244f1fbda4ed3978df60e0c5097e5b83e882
Clarence Manuel <rencedm112@gmail.com>
2023-04-11 22:26:58 +0800

    Removed neotree for telescope file browser

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -8,7 +8,23 @@ return {
         return
       end
 
-      plugin.setup({})
+      plugin.setup({
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                -- your custom normal mode mappings
+              },
+            },
+          },
+        },
+      })
 
       local builtins_ok, builtins = pcall(require, "telescope.builtin")
       if not builtins_ok then
---
+++ config/nvim/lua/plugins/telescope_file_browser.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").load_extension("file_browser")
+
+      local default_opts = require("defaults").keymap.opts
+      local set = vim.keymap.set
+
+      -- set("n", "<space>fb", ":Telescope file_browser<cr>", default_opts)
+      set(
+        "n",
+        "<space>fb",
+        ":Telescope file_browser path=%:p:h select_buffer=true<cr>",
+        default_opts
+      )
+    end,
+  },
+}
