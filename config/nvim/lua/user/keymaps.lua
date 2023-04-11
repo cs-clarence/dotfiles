94fdff2e173b2b7fd8f6bcdbcbbaac73ebdaf650
Clarence Manuel <rencedm112@gmail.com>
2023-04-11 21:57:04 +0800

    Update

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,5 @@
 -- Create a copy of keymap function
-local keymap = vim.keymap.set
+local set = vim.keymap.set
 
 local default_opts = { noremap = true, silent = true }
 -- local expr_opts = { noremap = true, expr = true, silent = true }
@@ -9,81 +9,81 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- ToggleTerm keymaps
-keymap("t", [[<ESC>]], [[<C-\><C-n>]], default_opts)
+set("t", [[<ESC>]], [[<C-\><C-n>]], default_opts)
 
 -- NeoToggleTree keymaps
-keymap("n", [[<leader>tt]], [[<cmd>Neotree toggle<cr>]], default_opts)
+set("n", [[<leader>tt]], [[<cmd>Neotree toggle<cr>]], default_opts)
 
 -- Better (?) window navigation
--- I disabled it because i prefer window comamnds starting with C-W after all
+-- I disabled it because i prefer window commands starting with C-W after all
 -- keymap("n", [[<C-k>]], [[<C-w>k]], default_opts)
 -- keymap("n", [[<C-j>]], [[<C-w>j]], default_opts)
 -- keymap("n", [[<C-l>]], [[<C-w>l]], default_opts)
 -- keymap("n", [[<C-h>]], [[<C-w>h]], default_opts)
 
 -- Window resize
-keymap("n", [[<A-k>]], [[<cmd>resize -1<cr>]], default_opts)
-keymap("n", [[<A-j>]], [[<cmd>resize +1<cr>]], default_opts)
-keymap("n", [[<A-h>]], [[<cmd>vertical resize -1<cr>]], default_opts)
-keymap("n", [[<A-l>]], [[<cmd>vertical resize +1<cr>]], default_opts)
+set("n", [[<A-k>]], [[<cmd>resize -1<cr>]], default_opts)
+set("n", [[<A-j>]], [[<cmd>resize +1<cr>]], default_opts)
+set("n", [[<A-h>]], [[<cmd>vertical resize -1<cr>]], default_opts)
+set("n", [[<A-l>]], [[<cmd>vertical resize +1<cr>]], default_opts)
 
 -- Buffer control
-keymap("n", [[<S-l>]], [[<cmd>bnext<cr>]], default_opts)
-keymap("n", [[<S-h>]], [[<cmd>bprevious<cr>]], default_opts)
-keymap("n", "<leader>bd", [[<cmd>bdelete<cr>]], default_opts)
+set("n", [[<S-l>]], [[<cmd>bnext<cr>]], default_opts)
+set("n", [[<S-h>]], [[<cmd>bprevious<cr>]], default_opts)
+set("n", "<leader>bd", [[<cmd>bdelete<cr>]], default_opts)
 
 -- Toggle ToggleTerm
-keymap("n", [[<leader>tr]], [[<cmd>ToggleTerm<cr>]], default_opts)
+set("n", [[<leader>tr]], [[<cmd>ToggleTerm<cr>]], default_opts)
 
 -- Tagbar
-keymap("n", [[<leader>tb]], [[<cmd>TagbarToggle<cr>]], default_opts)
+set("n", [[<leader>tb]], [[<cmd>TagbarToggle<cr>]], default_opts)
 
 -- Trouble
-keymap(
+set(
   "n",
   [[<leader>dd]],
   [[<cmd>TroubleToggle document_diagnostics<cr>]],
   default_opts
 )
 
-keymap(
+set(
   "n",
   [[<leader>dw]],
   [[<cmd>TroubleToggle workspace_diagnostics<cr>]],
   default_opts
 )
 
-keymap("n", [[<leader>dt]], [[<cmd>TroubleToggle todo<cr>]], default_opts)
+set("n", [[<leader>dt]], [[<cmd>TroubleToggle todo<cr>]], default_opts)
 
 -- Diagnostics
-keymap("n", [[<leader>d]], vim.diagnostic.open_float, default_opts)
-keymap("n", [[<leader>df]], vim.diagnostic.open_float, default_opts)
-keymap("n", [=[[d]=], vim.diagnostic.goto_prev, default_opts)
-keymap("n", [=[]d]=], vim.diagnostic.goto_next, default_opts)
+set("n", [[<leader>d]], vim.diagnostic.open_float, default_opts)
+set("n", [[<leader>df]], vim.diagnostic.open_float, default_opts)
+set("n", [=[[d]=], vim.diagnostic.goto_prev, default_opts)
+set("n", [=[]d]=], vim.diagnostic.goto_next, default_opts)
 
 -- Gitsigns
-keymap("n", [[<leader>cb]], [[<cmd>Gitsigns blame_line<cr>]], default_opts)
-keymap("n", [[<leader>cp]], [[<cmd>Gitsigns preview_hunk<cr>]], default_opts)
-keymap("n", [=[[c]=], [[<cmd>Gitsigns prev_hunk<cr>]], default_opts)
-keymap("n", [=[]c]=], [[<cmd>Gitsigns next_hunk<cr>]], default_opts)
+set("n", [[<leader>cb]], [[<cmd>Gitsigns blame_line<cr>]], default_opts)
+set("n", [[<leader>cp]], [[<cmd>Gitsigns preview_hunk<cr>]], default_opts)
+set("n", [=[[c]=], [[<cmd>Gitsigns prev_hunk<cr>]], default_opts)
+set("n", [=[]c]=], [[<cmd>Gitsigns next_hunk<cr>]], default_opts)
 
 -- Neotest
 local neotest_ok, neotest = pcall(require, "neotest")
 if neotest_ok then
   -- Test current function
-  keymap("n", [[<leader>rr]], function()
+  set("n", [[<leader>rr]], function()
     neotest.run.run()
   end, default_opts)
   -- Run all test in the current file
-  keymap("n", [[<leader>rf]], function()
+  set("n", [[<leader>rf]], function()
     neotest.run.run(vim.fn.expand("%"))
   end, default_opts)
   -- Run all tests in the current directory of the current file
-  keymap("n", [[<leader>rf]], function()
+  set("n", [[<leader>rf]], function()
     neotest.run.run(vim.fn.expand("%"))
   end, default_opts)
   -- Test the whole CWD
-  keymap("n", [[<leader>rc]], function()
+  set("n", [[<leader>rc]], function()
     neotest.run.run(vim.fn.getcwd())
   end, default_opts)
 end
