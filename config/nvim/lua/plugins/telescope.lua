0a3ab1f539760d69149089f8a43c48ceaf8f09e3
Clarence Manuel <rencedm112@gmail.com>
2023-05-03 18:00:16 +0800

    Update

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,16 @@ return {
         return
       end
 
+      local fb_actions_ok, fb_actions =
+        pcall(require, "telescope._extensions.file_browser.actions")
+
+      if not fb_actions_ok then
+        vim.notify(
+          "Failed to require telescope._extensions.file_browser.actions"
+        )
+        return
+      end
+
       plugin.setup({
         extensions = {
           file_browser = {
@@ -28,6 +38,40 @@ return {
 
             -- Show hidden files
             hidden = true,
+            mappings = {
+              ["i"] = {
+                ["<A-c>"] = fb_actions.create,
+                ["<S-CR>"] = fb_actions.create_from_prompt,
+                ["<A-r>"] = fb_actions.rename,
+                ["<A-m>"] = fb_actions.move,
+                ["<A-y>"] = fb_actions.copy,
+                ["<A-d>"] = fb_actions.remove,
+                ["<C-o>"] = fb_actions.open,
+                ["<C-g>"] = fb_actions.goto_parent_dir,
+                ["<C-e>"] = fb_actions.goto_home_dir,
+                ["<C-w>"] = fb_actions.goto_cwd,
+                ["<C-t>"] = fb_actions.change_cwd,
+                ["<C-f>"] = fb_actions.toggle_browser,
+                ["<C-h>"] = fb_actions.toggle_hidden,
+                ["<C-s>"] = fb_actions.toggle_all,
+                ["<bs>"] = fb_actions.backspace,
+              },
+              ["n"] = {
+                ["c"] = fb_actions.create,
+                ["r"] = fb_actions.rename,
+                ["m"] = fb_actions.move,
+                ["y"] = fb_actions.copy,
+                ["d"] = fb_actions.remove,
+                ["o"] = fb_actions.open,
+                ["g"] = fb_actions.goto_parent_dir,
+                ["e"] = fb_actions.goto_home_dir,
+                ["w"] = fb_actions.goto_cwd,
+                ["t"] = fb_actions.change_cwd,
+                ["f"] = fb_actions.toggle_browser,
+                ["h"] = fb_actions.toggle_hidden,
+                ["s"] = fb_actions.toggle_all,
+              },
+            },
           },
         },
       })
@@ -45,7 +89,7 @@ return {
 
       keymap("n", [[<leader>ff]], builtins.find_files, default_opts)
       keymap("n", [[<leader>fg]], builtins.live_grep, default_opts)
-      keymap("n", [[<leader>fb]], builtins.buffers, default_opts)
+      keymap("n", [[<leader>fd]], builtins.buffers, default_opts)
       keymap("n", [[<leader>fh]], builtins.help_tags, default_opts)
     end,
   },
