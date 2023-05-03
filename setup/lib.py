bb557b6b5ec772755ce7d1ac9279331f1b9f0b9a
Clarence Manuel <rencedm112@gmail.com>
2023-05-03 08:38:42 +0800

    Update alacritty

--- setup/lib.py
+++ setup/lib.py
@@ -1,7 +1,7 @@
 import re
 import subprocess
 from dataclasses import dataclass
-from typing import Literal, NotRequired, TypedDict
+from typing import Literal, NotRequired, Required, TypedDict
 
 import distro
 
@@ -11,34 +11,48 @@ WS = re.compile(r"\s+")
 
 
 class InstallJob:
-    pre_install_scripts: Scripts
+    pre_install_scripts: Scripts | None
     install_scripts: Scripts
-    post_install_scripts: Scripts
+    post_install_scripts: Scripts | None
 
     @staticmethod
-    def dnf(package_name: str, pre: Scripts, post: Scripts) -> "InstallJob":
+    def dnf(
+        package_name: str, pre: Scripts | None, post: Scripts | None
+    ) -> "InstallJob":
         return InstallJob(
             install=[f"sudo dnf install -y {package_name}"], pre=pre, post=post
         )
 
     @staticmethod
-    def script(script: str, pre: Scripts, post: Scripts) -> "InstallJob":
-        return InstallJob(install=[script])
+    def scripts(
+        scripts: Scripts, pre: Scripts | None, post: Scripts | None
+    ) -> "InstallJob":
+        return InstallJob(
+            install=scripts,
+            pre=pre,
+            post=post,
+        )
 
     @staticmethod
-    def snap_classic(package_name: str, pre: Scripts, post: Scripts) -> "InstallJob":
+    def snap_classic(
+        package_name: str, pre: Scripts | None, post: Scripts | None
+    ) -> "InstallJob":
         return InstallJob(
             install=[f"sudo snap install --classic {package_name}"], pre=pre, post=post
         )
 
     @staticmethod
-    def snap(package_name: str, pre: Scripts, post: Scripts) -> "InstallJob":
+    def snap(
+        package_name: str, pre: Scripts | None, post: Scripts | None
+    ) -> "InstallJob":
         return InstallJob(
             install=[f"sudo snap install {package_name}"], pre=pre, post=post
         )
 
     @staticmethod
-    def flatpak(package_name: str, pre: Scripts, post: Scripts) -> "InstallJob":
+    def flatpak(
+        package_name: str, pre: Scripts | None, post: Scripts | None
+    ) -> "InstallJob":
         return InstallJob(
             install=[
                 f"sudo flatpak install flathub {package_name}",
@@ -48,11 +62,12 @@ class InstallJob:
         )
 
     def exec(self) -> None:
-        for cmd in self.pre_install_scripts:
-            if isinstance(cmd, str):
-                subprocess.run(WS.split(cmd))
-            else:
-                subprocess.run(cmd)
+        if self.pre_install_scripts is not None:
+            for cmd in self.pre_install_scripts:
+                if isinstance(cmd, str):
+                    subprocess.run(WS.split(cmd))
+                else:
+                    subprocess.run(cmd)
 
         for cmd in self.install_scripts:
             if isinstance(cmd, str):
@@ -60,17 +75,18 @@ class InstallJob:
             else:
                 subprocess.run(cmd)
 
-        for cmd in self.post_install_scripts:
-            if isinstance(cmd, str):
-                subprocess.run(WS.split(cmd))
-            else:
-                subprocess.run(cmd)
+        if self.post_install_scripts is not None:
+            for cmd in self.post_install_scripts:
+                if isinstance(cmd, str):
+                    subprocess.run(WS.split(cmd))
+                else:
+                    subprocess.run(cmd)
 
     def __init__(
         self,
         install=[],
-        pre: Scripts = [],
-        post: Scripts = [],
+        pre: Scripts | None = None,
+        post: Scripts | None = None,
     ):
         self.pre_install_scripts = pre
         self.install_scripts = install
@@ -83,7 +99,7 @@ InstallType = Literal[
 
 
 class PackageName(TypedDict, total=False):
-    all: NotRequired[str]
+    all: Required[str]
     snap: NotRequired[str]
     flatpak: NotRequired[str]
     dnf: NotRequired[str]
@@ -99,7 +115,100 @@ class PackageInformation:
     post_install_scripts: Scripts | None = None
 
     def create_install_job(self) -> InstallJob:
-        pass
+        match self.install_type:
+            case "snap" | "snap-classic":
+                if self.package is None:
+                    raise Exception("Package name is required for snap packages")
+
+                pkg = ""
+                if isinstance(self.package, str):
+                    pkg = self.package
+                else:
+                    if "snap" in self.package:
+                        pkg = self.package["snap"]
+                    elif "all" in self.package:
+                        pkg = self.package["all"]
+                    else:
+                        raise Exception("Package name is required for snap packages")
+
+                if self.install_type == "snap":
+                    return InstallJob.snap(
+                        pkg,
+                        pre=self.pre_install_scripts,
+                        post=self.post_install_scripts,
+                    )
+                elif self.install_type == "snap-classic":
+                    return InstallJob.snap_classic(
+                        pkg,
+                        pre=self.pre_install_scripts,
+                        post=self.post_install_scripts,
+                    )
+                else:
+                    raise Exception("Not implemented")
+
+            case "native-package-manager":
+                distro_id = distro.id()
+
+                match distro_id:
+                    case "fedora":
+                        if self.package is None:
+                            raise Exception(
+                                "Package name is required for native package manager"
+                            )
+
+                        pkg = ""
+                        if isinstance(self.package, str):
+                            pkg = self.package
+                        else:
+                            if "dnf" in self.package:
+                                pkg = self.package["dnf"]
+                            elif "all" in self.package:
+                                pkg = self.package["all"]
+                            else:
+                                raise Exception(
+                                    "Package name is required for native package manager"
+                                )
+
+                        return InstallJob.snap(
+                            pkg,
+                            pre=self.pre_install_scripts,
+                            post=self.post_install_scripts,
+                        )
+                    case _:
+                        raise Exception("Not implemented")
+
+            case "flatpak":
+                if self.package is None:
+                    raise Exception("Package name is required for flatpak")
+
+                pkg = ""
+                if isinstance(self.package, str):
+                    pkg = self.package
+                else:
+                    if "flatpak" in self.package:
+                        pkg = self.package["flatpak"]
+                    elif "all" in self.package:
+                        pkg = self.package["all"]
+                    else:
+                        raise Exception("Package name is required for flatpak")
+
+                return InstallJob.flatpak(
+                    pkg,
+                    pre=self.pre_install_scripts,
+                    post=self.post_install_scripts,
+                )
+
+            case "script":
+                if self.install_scripts is None:
+                    raise Exception("Install scripts are required to install a script")
+
+                return InstallJob.scripts(
+                    self.install_scripts,
+                    pre=self.pre_install_scripts,
+                    post=self.post_install_scripts,
+                )
+            case _:
+                raise Exception("Not implemented")
 
 
-pkg = PackageInformation
+Pkg = PackageInformation
--- setup/main.py
+++ setup/main.py
@@ -3,42 +3,22 @@
 from lib import *
 
 pkgs = [
-    pkg("nvim", package="neovim"),
-    pkg("git"),
-    pkg("clang"),
-    pkg("cmake"),
-    pkg("shfmt"),
-    pkg("alacritty"),
-    pkg("firefox"),
-    pkg("qbittorrent"),
-    pkg("vlc"),
-    pkg("flutter", install_type="snap-classic"),
+    Pkg("nvim", package="neovim"),
+    Pkg("git"),
+    Pkg("clang"),
+    Pkg("cmake"),
+    Pkg("shfmt"),
+    Pkg("alacritty"),
+    Pkg("firefox"),
+    Pkg("qbittorrent"),
+    Pkg("vlc"),
+    Pkg("flutter", install_type="snap-classic"),
 ]
 
 
 def main():
-    jobs = [
-        InstallJob(
-            "snap",
-            post_install={"fedora": ["sudo ls -s /var/lib/snapd/snap /snap"]},
-            dnf="snapd",
-        ),
-        InstallJob("nvim", dnf="neovim"),
-        InstallJob("git", dnf="git"),
-        InstallJob("clang", dnf="clang"),
-        InstallJob("cmake", dnf="cmake"),
-        InstallJob("shfmt", dnf="shfmt"),
-        InstallJob("kitty", dnf="kitty"),
-        InstallJob("firefox", dnf="firefox"),
-        InstallJob("qbittorent", dnf="qbittorrent"),
-        InstallJob("vlc", dnf="vlc"),
-        InstallJob.snap_classic(
-            "flutter",
-            snap="flutter",
-        ),
-        InstallJob.snap_classic("telegram-desktop", snap="telegram-desktop"),
-    ]
-    run_install_jobs(jobs)
+    for pkg in pkgs:
+        pkg.create_install_job().exec()
 
 
 if __name__ == "__main__":
