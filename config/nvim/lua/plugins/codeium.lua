214790ed89087865c1c8088af46ec6fb765480bb
Clarence Manuel <rencedm112@gmail.com>
2024-03-20 19:16:19 +0800

    fix: add cond to codeium

--- config/nvim/lua/plugins/codeium.lua
+++ config/nvim/lua/plugins/codeium.lua
@@ -1,18 +1,22 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
+    {
+        cond = function()
+            return not vim.g.vscode
+        end,
+        "Exafunction/codeium.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+
+        config = function()
+            local codeium_ok, codeium = pcall(require, "codeium")
+
+            if not codeium_ok then
+                vim.notify("Codeium is not successfully require")
+            end
+
+            codeium.setup({})
+        end,
     },
-    config = function()
-      local codeium_ok, codeium = pcall(require, "codeium")
-
-      if not codeium_ok then
-        vim.notify("Codeium is not successfully require")
-      end
-
-      codeium.setup({})
-    end,
-  },
 }
