75ac731e18dc763221bf51f68938df6fc58bd35e
Clarence Manuel <rencedm112@gmail.com>
2024-06-12 07:54:48 +0800

    feat: disable oil as default file explorer

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -11,7 +11,40 @@ return {
                 vim.notify("Failed to load oil")
                 return
             end
-            oil.setup({})
+            -- reference: https://github.com/stevearc/oil.nvim?tab=readme-ov-file#options
+            oil.setup({
+                default_file_explorer = false,
+
+                keymaps = {
+                    ["g?"] = "actions.show_help",
+                    ["<CR>"] = "actions.select",
+                    ["<C-s>"] = {
+                        "actions.select_split",
+                        opts = { vertical = true },
+                    },
+                    ["<C-h>"] = {
+                        "actions.select_split",
+                        opts = { horizontal = true },
+                    },
+                    ["<C-t>"] = {
+                        "actions.select_split",
+                        opts = { tab = true },
+                    },
+                    ["<C-p>"] = "actions.preview",
+                    ["<C-c>"] = "actions.close",
+                    ["<C-l>"] = "actions.refresh",
+                    ["-"] = "actions.parent",
+                    ["_"] = "actions.open_cwd",
+                    ["`"] = "actions.cd",
+                    ["~"] = { "actions.cd", opts = { scope = "tab" } },
+                    ["gs"] = "actions.change_sort",
+                    ["gx"] = "actions.open_external",
+                    ["g."] = "actions.toggle_hidden",
+                    ["g\\"] = "actions.toggle_trash",
+                },
+                -- Set to false to disable all of the above keymaps
+                use_default_keymaps = true,
+            })
         end,
     },
 }
